02ea9ca7c51a3a99483b8f355cb3615d9a748d16
root <root@instance-1.c.comsw4995-project.internal>
2018-12-04 05:03:51 +0000

    run drmm script

--- examples/HotpotQA/run_drmm.sh
+++ examples/HotpotQA/run_drmm.sh
@@ -2,9 +2,9 @@ cd ../../
 
 currpath=`pwd`
 # train the model
-python matchzoo/main.py --phase train --model_file ${currpath}/examples/hotpotqa/config/drmm_hotpotqa.config
+#python3 matchzoo/main.py --phase train --model_file ${currpath}/examples/HotpotQA/config/drmm_hotpotqa.config
 
 
 # predict with the model
 
-python matchzoo/main.py --phase predict --model_file ${currpath}/examples/hotpotqa/config/drmm_hotpotqa.config
+python3 matchzoo/main.py --phase predict --model_file ${currpath}/examples/HotpotQA/config/drmm_hotpotqa.config
